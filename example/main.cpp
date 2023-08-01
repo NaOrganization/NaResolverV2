c5edb1555db123135d92bca0890c345dbdda4a6c
MidTerm <83219310+MidTerm-CN@users.noreply.github.com>
2023-08-01 18:22:17 +0800

    Update main.cpp

--- example/main.cpp
+++ example/main.cpp
@@ -26,15 +26,14 @@ int WINAPI DllMain(HINSTANCE hinstDLL, DWORD fdwReason, LPVOID lpvReserved)
 
 		if (!MonoResolver->Setup(config))
 			printf("Failed to setup I2Hrame!\n");
-		MonoClass *klass = MonoResolver->GetClassEx("UnityEngine.CoreModule", "UnityEngine", "GameObject");
-		printf("(Il2CppResolver->GetClassEx) Class: %p\n", klass);
-		klass = MonoResolver->GetClass("(UnityEngine.CoreModule)UnityEngine.GameObject");
-		printf("(Il2CppResolver->GetClass) Class: %p\n", klass);
-		MonoMethodPointer method = MonoResolver->GetMethod(klass, "UnityEngine.Transform get_transform()");
-		printf("(Il2CppResolver->GetMethod) Method: %p\n", method);
-
-		klass = MonoResolver->GetClass("(UnityEngine.CoreModule)UnityEngine.GameObjct");
-		printf("(Error Demonstration) Class: %p\n", klass);
+		MonoClass *klass = MonoResolver->GetClassEx("UnityEngine.CoreModule", "UnityEngine", "Camera");
+		printf("Class: %p\n", klass);
+		MonoMethodPointer method = MonoResolver->GetMethod(klass, "UnityEngine.Camera get_main()");
+		printf("Method: %p\n", method);
+		auto func = (void * (*)(MonoMethod*))method;
+		void* camera = func(nullptr);
+		printf("Camera: %p\n", camera);
+		// if your camera was null, it means that the game is not loaded yet
 		
 		MonoResolver->Destroy();
 		free(MonoResolver);
